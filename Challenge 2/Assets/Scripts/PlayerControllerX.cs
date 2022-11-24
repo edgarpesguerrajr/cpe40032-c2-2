6e1ecb0f79b88196f2b3923531681a941af24fdf
Edgar P. Esguerra Jr <jedesguerra37@gmail.com>
2022-11-24 16:52:49 +0800

    cpe40032-c2-2
    FINAL

---
+++ Challenge 2/Assets/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fullCooldown = 0.5f;
+    private float currentCooldown = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog if cooldown has passed
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > currentCooldown)
+        {
+            currentCooldown = Time.time + fullCooldown;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+        }
+    }
+
+}
